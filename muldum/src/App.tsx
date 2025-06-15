02d7645caeab3b7f61a9cf91d5f859781e804583
dpqlsn <a01037111209@gmail.com>
2025-06-15 13:15:05 +0900

    feat (#7) :: 공지사항 부분 클릭 시 이동

--- muldum/src/App.tsx
+++ muldum/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './page/Main/Main';
+import Notice from './page/Notice/Notice';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/notice" element={<Notice />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ muldum/src/page/Notice/Notice.tsx
@@ -0,0 +1,10 @@
+import * as _ from './style';
+import '../../App.css';
+
+export default function Notice(){
+    return(
+        <_.Container>
+            <p>gg</p>
+        </_.Container>
+    );
+}
