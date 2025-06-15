410f863f0cf017b4e443841272d384539a4276fb
dpqlsn <a01037111209@gmail.com>
2025-06-15 13:30:17 +0900

    feat (#8) :: 공지사항 타이틀

--- muldum/src/page/Notice/Notice.tsx
+++ muldum/src/page/Notice/Notice.tsx
@@ -1,10 +1,21 @@
 import * as _ from './style';
 import '../../App.css';
+import Search from '../../assets/icon/Search.svg';
+import Add from '../../assets/icon/Add.svg';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default function Notice(){
     return(
         <_.Container>
-            <p>gg</p>
+            <NavBar />
+            <_.Wrapper>
+                <_.Title>공지사항</_.Title>
+                <_.SearchBar>
+                    <img src={Search} alt='Search' />
+                    공지사항 검색
+                </_.SearchBar>
+                <img src={Add} alt='Add' />
+            </_.Wrapper>
         </_.Container>
     );
 }
--- muldum/src/page/Notice/style.ts
+++ muldum/src/page/Notice/style.ts
@@ -3,4 +3,37 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
     width: 100%;
     height: 100vh;
+    margin-left: 12%;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 1%;
+    margin-top: 2%;
+    width: 90%;
+    gap: 26%;
+`;
+
+export const Title = styled.div`
+    display: flex;
+    justify-content: left;
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: #1D1D1D;
+`;
+
+export const SearchBar = styled.div`
+    display: flex;
+    align-items: center;
+    width: 30%;
+    height: 2vh;
+    padding: 1%;
+    background-color: #FAFAFA;
+    color: #909090;
+    border-radius: 12px;
+    gap: 4%;
+    margin-left: 2%;
+    font-size: 1rem;
+
 `;
