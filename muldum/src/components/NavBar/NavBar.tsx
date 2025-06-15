7eea12b97b54abcd6e63c9b0a27a1de99cebb996
dpqlsn <a01037111209@gmail.com>
2025-06-15 15:40:54 +0900

    fix (#4) :: 사이드바 항목 클릭 시 이동

--- muldum/src/components/NavBar/NavBar.tsx
+++ muldum/src/components/NavBar/NavBar.tsx
@@ -1,32 +1,36 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { IconMenu } from './IconMenu';
 import * as _ from './style';
 import '../../App.css';
 
 export default function NavBar() {
     const [activeMenu, setActiveMenu] = useState('');
+    const navigate = useNavigate();
 
     return (
         <_.MainArea>
             {IconMenu.map((item) => {
                 const isActive = activeMenu === item.label;
-
                 const TagComponent =
                     item.label === '로그인'
-                    ? _.LoginTag
-                    : item.label === '설정'
-                    ? _.SettingTag
-                    : _.TagArea;
+                        ? _.LoginTag
+                        : item.label === '설정'
+                        ? _.SettingTag
+                        : _.TagArea;
 
                 return (
                     <TagComponent
-                    key={item.label}
-                    onClick={() => setActiveMenu(item.label)}
-                    isActive={isActive} 
+                        key={item.label}
+                        onClick={() => {
+                            setActiveMenu(item.label);
+                            navigate(item.path);
+                        }}
+                        isActive={isActive}
                     >
                         <_.Icon
-                        src={isActive ? item.iconActive : item.icon}
-                        alt={item.label}
+                            src={isActive ? item.iconActive : item.icon}
+                            alt={item.label}
                         />
                         <_.Text isActive={isActive}>{item.label}</_.Text>
                     </TagComponent>
