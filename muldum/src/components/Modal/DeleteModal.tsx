816e370f0e310ee0d792d342e07ff8c90f0990f9
dpqlsn <a01037111209@gmail.com>
2025-06-16 19:38:34 +0900

    feat (#2) :: 삭제 당부 모달

---
+++ muldum/src/components/Modal/DeleteModal.tsx
@@ -0,0 +1,24 @@
+import * as _ from './style';
+import '../../App.css';
+
+interface Props {
+    onCancel: () => void;
+    onConfirm: () => void;
+}
+
+export default function DeleteModal({ onCancel, onConfirm }: Props) {
+    return (
+        <_.Container>
+            <_.Modal>
+                <_.TextArea>
+                    <_.Title>정말 삭제하시겠습니까?</_.Title>
+                    <_.SubTitle>삭제한 내용은 되돌릴 수 없습니다.</_.SubTitle>
+                </_.TextArea>
+                <_.ButtonGroup>
+                    <_.Yes onClick={onConfirm}>네</_.Yes>
+                    <_.No onClick={onCancel}>취소</_.No>
+                </_.ButtonGroup>
+            </_.Modal>
+        </_.Container>
+    );
+}
