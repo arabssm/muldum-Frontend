cec8d4fec8ed9f38e6c8f297187e8a668a20f80d
dpqlsn <a01037111209@gmail.com>
2025-06-16 13:28:03 +0900

    fix (#9) :: 이전페이지로 돌아가기

--- muldum/src/components/NotFound/NotFound.tsx
+++ muldum/src/components/NotFound/NotFound.tsx
@@ -10,7 +10,7 @@ export default function Notfound() {
             <NavBar />
             <_.Error>404</_.Error>
             <_.Title>요청하신 화면을 찾을 수 없습니다</_.Title>
-            <_.SubTitle onClick={() => navigate('/')}>홈으로 돌아가기</_.SubTitle>
+            <_.SubTitle onClick={() => navigate(-1)}>이전 페이지로 돌아가기</_.SubTitle>
         </_.Container>
     );
 }
