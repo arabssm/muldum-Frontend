232bfc7926afdf79aecee48a55f3e4d7194a9d1d
dpqlsn <a01037111209@gmail.com>
2025-06-16 13:58:02 +0900

    fix (#6) :: + 버튼 클릭 시 페이지 띄우기

--- muldum/src/App.tsx
+++ muldum/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './page/Main/Main';
 import Notice from './page/Notice/Notice/Notice';
 import DetailNotice from './page/Notice/Detail/DetailNotice';
+import CreateNotice from './page/Notice/Create/CreateNotice';
 import NotFound from './components/NotFound/NotFound'
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Main />} />
         <Route path="/notice" element={<Notice />} />
         <Route path="/notice/:id" element={<DetailNotice />} />
+        <Route path="/create-notice" element={<CreateNotice />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ muldum/src/page/Notice/Create/CreateNotice.tsx
@@ -0,0 +1,11 @@
+import * as _ from './style';
+import '../../../App.css';
+import NavBar from '../../../components/NavBar/NavBar';
+
+export default function CreateNotice(){
+    return(
+        <_.Container>
+            <NavBar />
+        </_.Container>
+    );
+}
