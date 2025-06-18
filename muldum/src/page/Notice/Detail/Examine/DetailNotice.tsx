4def2df0e9ca4f2ac8ccccaded80ec140b1118fa
dpqlsn <a01037111209@gmail.com>
2025-06-18 17:42:27 +0900

    feat (#9) :: 수정완료 모달 띄우기

--- muldum/src/page/Notice/Detail/Examine/DetailNotice.tsx
+++ muldum/src/page/Notice/Detail/Examine/DetailNotice.tsx
@@ -16,54 +16,54 @@ export default function NoticeDetail() {
     const [showConfirmModal, setShowConfirmModal] = useState(false);
     const notice: Notice | undefined = data.find(item => item.idx === Number(id));
 
-    if (!notice) return <Notfound />;
+if (!notice) return <Notfound />;
 
-    const handleDelete = () => {
-        setShowModal(false);
-        setShowConfirmModal(true);
+const handleDelete = () => {
+    setShowModal(false);
+    setShowConfirmModal(true);
 
-        setTimeout(() => {
-            setShowConfirmModal(false);
-            navigate('/notice');
-        }, 5000);
-    };
+    setTimeout(() => {
+        setShowConfirmModal(false);
+        navigate('/notice');
+    }, 5000);
+};
 
-    return (
-        <_.Container>
-            <NavBar />
-            <_.Wrapper>
-                <_.PageTitle>{notice.title}</_.PageTitle>
-                <_.AdditionRow>
-                    <_.AdditionLeft>
-                        <_.Addition>등록일: {notice.formattedDate}</_.Addition>
-                        <_.Addition>작성자: {notice.author}</_.Addition>
-                    </_.AdditionLeft>
-                    <_.ButtonGroup>
-                        <button onClick={() => setShowModal(true)}>삭제하기</button>
-                        <button onClick={() => navigate(`/notice/edit/${notice.idx}`)}>수정하기</button>
-                    </_.ButtonGroup>
-                </_.AdditionRow>
-                <_.Image src={notice.image} alt="notice" />
-                <_.ImageCaption>{notice.imageCaption}</_.ImageCaption>
-                <_.Content>
-                    {makeDocument(notice.content.join('\n'))}
-                </_.Content>
-                <_.BackButton onClick={() => navigate(-1)}>이전</_.BackButton>
-            </_.Wrapper>
+return (
+    <_.Container>
+        <NavBar />
+        <_.Wrapper>
+            <_.PageTitle>{notice.title}</_.PageTitle>
+            <_.AdditionRow>
+                <_.AdditionLeft>
+                    <_.Addition>등록일: {notice.formattedDate}</_.Addition>
+                    <_.Addition>작성자: {notice.author}</_.Addition>
+                </_.AdditionLeft>
+                <_.ButtonGroup>
+                    <button onClick={() => setShowModal(true)}>삭제하기</button>
+                    <button onClick={() => navigate(`/notice/edit/${notice.idx}`)}>수정하기</button>
+                </_.ButtonGroup>
+            </_.AdditionRow>
+            <_.Image src={notice.image} alt="notice" />
+            <_.ImageCaption>{notice.imageCaption}</_.ImageCaption>
+            <_.Content>
+                {makeDocument(notice.content.join('\n'))}
+            </_.Content>
+            <_.BackButton onClick={() => navigate(-1)}>이전</_.BackButton>
+        </_.Wrapper>
 
-            {showModal && (
-                <DeleteModal
-                    onCancel={() => setShowModal(false)}
-                    onConfirm={handleDelete}
-                />
-            )}
+        {showModal && (
+            <DeleteModal
+                onCancel={() => setShowModal(false)}
+                onConfirm={handleDelete}
+            />
+        )}
 
-            {showConfirmModal && (
-                <ConfirmDeleteModal onClose={() => {
-                    setShowConfirmModal(false);
-                    navigate('/notice');
-                }} />
-            )}
-        </_.Container>
-    );
+        {showConfirmModal && (
+            <ConfirmDeleteModal onClose={() => {
+                setShowConfirmModal(false);
+                navigate('/notice');
+            }} />
+        )}
+    </_.Container>
+);
 }
