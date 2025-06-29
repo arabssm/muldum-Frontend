54842558ff7ade5c822dbb686b85c1e3db530878
dpqlsn <a01037111209@gmail.com>
2025-06-29 14:04:23 +0900

    fix (#2) :: 컴포넌트로 따로 빼기

--- muldum/src/page/Item/Approval/Approval.tsx
+++ muldum/src/page/Item/Approval/Approval.tsx
@@ -3,7 +3,8 @@ import * as _ from './style';
 import NavBar from '@_navbar/NavBar';
 import '@_styles';
 import ApprovalList from '@_components/Item/List/ApprovalList';
-import CLUBS from './ClubList';
+import Clubs from './ClubList';
+import LongShot from './LongShot';
 
 export default function Approval() {
     const [filter, setFilter] = useState<'가능' | '불가능'>('가능');
@@ -19,14 +20,13 @@ export default function Approval() {
         setSelectedClub(club === selectedClub ? null : club);
     };
 
-    const filteredClubs = CLUBS;
+    const filteredClubs = Clubs;
 
     return (
         <_.Container>
             <NavBar />
             <_.Title>전공동아리 물품 승인</_.Title>
             <_.Subtitle>학생들이 신청한 물품들을 확인해요</_.Subtitle>
-
             <_.ButtonArea>
                 <_.ApprovalButton onClick={() => handleFilter('가능')} active={filter === '가능'}>
                     승인 가능
@@ -35,7 +35,6 @@ export default function Approval() {
                     승인 불가능
                 </_.ApprovalButton>
             </_.ButtonArea>
-
             {filter === '가능' ? (
                 <>
                     <_.ClubArea>
@@ -49,12 +48,10 @@ export default function Approval() {
                             </_.ClubName>
                         ))}
                     </_.ClubArea>
-
                     <_.AddonsArea>
                         <_.Addons>다운로드</_.Addons>
                         <_.Addons>전체선택</_.Addons>
                     </_.AddonsArea>
-
                     {selectedClub ? (
                         <ApprovalList />
                     ) : (
@@ -63,33 +60,10 @@ export default function Approval() {
                 </>
             ) : (
                 <>
-                    <_.RejectReasonArea>
-                        <_.RejectButton 
-                            active={rejectReason === '구입처를 잘못 입력한 물품'}
-                            onClick={() => setRejectReason('구입처를 잘못 입력한 물품')}
-                        >
-                            구입처를 잘못 입력한 물품
-                        </_.RejectButton>
-                        <_.RejectButton 
-                            active={rejectReason === '구매 사유가 적절하지 않은 물품'}
-                            onClick={() => setRejectReason('구매 사유가 적절하지 않은 물품')}
-                        >
-                            구매 사유가 적절하지 않은 물품
-                        </_.RejectButton>
-                        <_.RejectButton 
-                            active={rejectReason === '가격이 과도하게 많이 나가는 물품'}
-                            onClick={() => setRejectReason('가격이 과도하게 많이 나가는 물품')}
-                        >
-                            가격이 과도하게 많이 나가는 물품
-                        </_.RejectButton>
-                        <_.RejectButton 
-                            active={rejectReason === '기타사유'}
-                            onClick={() => setRejectReason('기타사유')}
-                        >
-                            기타사유
-                        </_.RejectButton>
-                    </_.RejectReasonArea>
-
+                    <LongShot 
+                        rejectReason={rejectReason}
+                        setRejectReason={setRejectReason}
+                    />
                     <_.AddonsArea>
                         <_.Addons>다운로드</_.Addons>
                         <_.Addons>전체선택</_.Addons>
@@ -97,7 +71,6 @@ export default function Approval() {
                     <ApprovalList />
                 </>
             )}
-
             <_.ButtonGroup>
                 <_.ApplyButton>승인하기</_.ApplyButton>
                 <_.ApplyNobutton>거절하기</_.ApplyNobutton>
---
+++ muldum/src/page/Item/Approval/LongShot.tsx
@@ -0,0 +1,26 @@
+import * as _ from './style';
+import '@_styles';
+import  Props from './type';
+
+const Reason = [
+    '구입처를 잘못 입력한 물품',
+    '구매 사유가 적절하지 않은 물품',
+    '가격이 과도하게 많이 나가는 물품',
+    '기타사유',
+];
+
+export default function RejectReasonSelector({ rejectReason, setRejectReason }: Props) {
+    return (
+        <_.RejectReasonArea>
+            {Reason.map((reason) => (
+                <_.RejectButton
+                    key={reason}
+                    active={rejectReason === reason}
+                    onClick={() => setRejectReason(reason)}
+                >
+                    {reason}
+                </_.RejectButton>
+            ))}
+        </_.RejectReasonArea>
+    );
+}
