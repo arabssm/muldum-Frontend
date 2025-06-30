0d89370fabe81e4221f6809c84871b8914960e04
dpqlsn <a01037111209@gmail.com>
2025-07-01 00:36:59 +0900

    feat (#7) :: 거절하기 모달

--- muldum/src/page/Item/Approval/Approval.tsx
+++ muldum/src/page/Item/Approval/Approval.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as _ from './style';
-import NavBar from '@_navbar/NavBar';
 import '@_styles';
+import NavBar from '@_navbar/NavBar';
 import ApprovalList from '@_components/Item/List/ApprovalList';
 import Clubs from './ClubList';
 import LongShot from './LongShot';
 import Filter from './Filter';
 import ClubSelector from './ClubSelector';
+import RejectModal from '@_modal/Approval/Rejectmodal';
 import ApprovalModal from '@_modal/Approval/ApprovalModal';
 
 export default function Approval() {
@@ -15,7 +16,8 @@ export default function Approval() {
     const [selectedClub, setSelectedClub] = useState<string | null>(null);
     const [rejectReason, setRejectReason] = useState<string>('구입처를 잘못 입력한 물품');
     const [selectAll, setSelectAll] = useState(false);
-    const [showModal, setShowModal] = useState(false);
+    const [showApproveModal, setShowApproveModal] = useState(false);
+    const [showRejectModal, setShowRejectModal] = useState(false);
     const navigate = useNavigate();
 
     const handleFilter = (type: '가능' | '불가능') => {
@@ -29,64 +31,78 @@ export default function Approval() {
     };
 
     const handleApproveClick = () => {
-        setShowModal(true);
+        setShowApproveModal(true);
     };
 
-    const closeModal = () => {
-        setShowModal(false);
+    const handleRejectClick = () => {
+        setShowRejectModal(true);
+    };
+
+    const closeApproveModal = () => {
+        setShowApproveModal(false);
+        navigate('/project-choice');
+    };
+
+    const closeRejectModal = () => {
+        setShowRejectModal(false);
+        navigate('/project-choice');
     };
 
     return (
         <_.Container>
-            <NavBar />
-            <_.Title>전공동아리 물품 승인</_.Title>
-            <_.Subtitle>학생들이 신청한 물품들을 확인해요</_.Subtitle>
+        <NavBar />
+        <_.Title>전공동아리 물품 승인</_.Title>
+        <_.Subtitle>학생들이 신청한 물품들을 확인해요</_.Subtitle>
 
-            <Filter filter={filter} setFilter={handleFilter} />
+        <Filter filter={filter} setFilter={handleFilter} />
 
-            {filter === '가능' ? (
-                <>
-                    <ClubSelector 
-                        clubs={Clubs} 
-                        selectedClub={selectedClub} 
-                        setSelectedClub={setSelectedClub} 
-                    />
-                    <_.AddonsArea>
-                        <_.Addons onClick={handleSelectAll}>
-                            {selectAll ? '전체해제' : '전체선택'}
-                        </_.Addons>
-                        <_.Addons>다운로드</_.Addons>
-                    </_.AddonsArea>
-                    {selectedClub ? (
-                        <ApprovalList selectAll={selectAll} />
-                    ) : (
-                        <_.Null>물품승인을 할 동아리를 선택해주세요</_.Null>
-                    )}
-                </>
+        {filter === '가능' ? (
+            <>
+            <ClubSelector
+                clubs={Clubs}
+                selectedClub={selectedClub}
+                setSelectedClub={setSelectedClub}
+            />
+            <_.AddonsArea>
+                <_.Addons onClick={handleSelectAll}>
+                {selectAll ? '전체해제' : '전체선택'}
+                </_.Addons>
+                <_.Addons>다운로드</_.Addons>
+            </_.AddonsArea>
+            {selectedClub ? (
+                <ApprovalList selectAll={selectAll} />
             ) : (
-                <>
-                    <LongShot rejectReason={rejectReason} setRejectReason={setRejectReason} />
-                    <_.AddonsArea>
-                        <_.Addons onClick={handleSelectAll}>
-                            {selectAll ? '전체해제' : '전체선택'}
-                        </_.Addons>
-                        <_.Addons>전체선택</_.Addons>
-                    </_.AddonsArea>
-                    <ApprovalList selectAll={selectAll} />
-                </>
+                <_.Null>물품승인을 할 동아리를 선택해주세요</_.Null>
             )}
-            <_.ButtonGroup>
-                <_.ApplyButton onClick={handleApproveClick}>승인하기</_.ApplyButton>
-                <_.ApplyNobutton>거절하기</_.ApplyNobutton>
-            </_.ButtonGroup>
-            {showModal && (
-                <ApprovalModal 
-                    onClose={() => {
-                        closeModal();
-                        navigate('/project-choice');
-                    }}
-                />
-            )}
+            </>
+        ) : (
+            <>
+            <LongShot rejectReason={rejectReason} setRejectReason={setRejectReason} />
+            <_.AddonsArea>
+                <_.Addons onClick={handleSelectAll}>
+                {selectAll ? '전체해제' : '전체선택'}
+                </_.Addons>
+                <_.Addons>전체선택</_.Addons>
+            </_.AddonsArea>
+            <ApprovalList selectAll={selectAll} />
+            </>
+        )}
+        <_.ButtonGroup>
+            <_.ApplyButton onClick={handleApproveClick}>승인하기</_.ApplyButton>
+            <_.ApplyNobutton onClick={handleRejectClick}>거절하기</_.ApplyNobutton>
+        </_.ButtonGroup>
+
+        {showApproveModal && (
+            <ApprovalModal
+            onClose={closeApproveModal}
+            />
+        )}
+
+        {showRejectModal && (
+            <RejectModal
+            onClose={closeRejectModal}
+            />
+        )}
         </_.Container>
     );
 }
