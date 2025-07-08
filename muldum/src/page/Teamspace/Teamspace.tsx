b3d1c4ab89fad0b1beff27ec9d9935751839eceb
dpqlsn <a01037111209@gmail.com>
2025-07-09 08:40:26 +0900

    fix (#2) :: 스타일 수정

--- muldum/src/page/Teamspace/Teamspace.tsx
+++ muldum/src/page/Teamspace/Teamspace.tsx
@@ -1,11 +1,15 @@
 import * as _ from './style';
+import '@_styles'
 import NavBar from '@_components/NavBar/NavBar';
 
 export default function Teamspace(){
     return(
         <_.Container>
             <NavBar />
-
+            <_.Wrapper>
+                <_.Title>동아리 팀원들을 배치해요</_.Title>
+                <_.Subtitle>각 동아리 별로 팀원들을 배치하고 관리해요</_.Subtitle>
+            </_.Wrapper>
         </_.Container>
     )
 }
