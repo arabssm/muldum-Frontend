bf34a526810130af6d692c7ed5fb7e2de28ffbeb
dpqlsn <a01037111209@gmail.com>
2025-07-09 08:25:15 +0900

    feature (#1) :: 팀스페이스 페이지로 이동

---
+++ muldum/src/page/Teamspace/Teamspace.tsx
@@ -0,0 +1,11 @@
+import * as _ from './style';
+import NavBar from '@_components/NavBar/NavBar';
+
+export default function Teamspace(){
+    return(
+        <_.Container>
+            <NavBar />
+
+        </_.Container>
+    )
+}
