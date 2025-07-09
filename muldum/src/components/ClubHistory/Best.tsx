339ad999d5c38e226bcd8ebc1af0121940727279
dpqlsn <a01037111209@gmail.com>
2025-07-09 12:06:46 +0900

    feat (#6) :: 상위팀 스타일

--- muldum/src/components/ClubHistory/Best.tsx
+++ muldum/src/components/ClubHistory/Best.tsx
@@ -1,9 +1,27 @@
 import * as _ from './style';
+import temporary from '@_assets/icon/temporary.svg';
 
 export default function Best(){
     return (
         <_.Container>
-            dd
+            <_.Box>
+                <_.award>대상</_.award>
+                <_.Group>
+                    <_.temporaryicon src={temporary} alt='icon' /> 
+                    <_.ClubName>밤돌이로</_.ClubName>
+                </_.Group>
+                <_.explanation>대서양 한가운데에서 밤돌이로를 외치다</_.explanation>
+                <_.Detail>자세히 보기</_.Detail>
+            </_.Box>
+            <_.Box>
+                <_.award>대상</_.award>
+                <_.Group>
+                    <_.temporaryicon src={temporary} alt='icon' /> 
+                    <_.ClubName>밤돌이로</_.ClubName>
+                </_.Group>
+                <_.explanation>대서양 한가운데에서 밤돌이로를 외치다</_.explanation>
+                <_.Detail>자세히 보기</_.Detail>
+            </_.Box>
         </_.Container>
     )
 }
--- muldum/src/components/ClubHistory/style.ts
+++ muldum/src/components/ClubHistory/style.ts
@@ -1,5 +1,62 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
+    display: flex;
+    gap: 3%;
     width: 100%;
+`;
+
+export const Box = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 20%;
+    padding: 3%;
+    background-color: #FAFAFA;
+    border-radius: 0.3rem;
+    gap: 10%;
+`;
+
+export const award = styled.div`
+    color: #FF9B62;
+    font-size: 0.8rem;
+    margin-bottom: 6%;
+    font-size: 1rem;
+`;
+
+export const ClubName = styled.div`
+    font-size: 1.5rem;
+    font-weight: 500;
+    color: #1D1D1D;
+`;
+
+export const temporaryicon = styled.img`
+    width: 20%;
+    cursor: pointer;
+`;
+
+export const explanation = styled.div`
+    font-size: 1rem;
+    color: #909090;
+`;
+
+export const Detail = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 1rem;
+    width: 100%;
+    padding: 5% 0% 4% 2%;
+    border-radius: 1rem;
+    background-color: #FF9B62;
+    color: #fff;
+    margin-top: 10%;
+    cursor: pointer;
+`;
+
+export const Group = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 4%;
+    margin-bottom: 6%;
 `;
