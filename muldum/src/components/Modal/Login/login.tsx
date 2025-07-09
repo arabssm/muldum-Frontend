184d82717eca686ea54a965204abaf28fe359aea
dpqlsn <a01037111209@gmail.com>
2025-07-09 19:48:54 +0900

    feat :: 로그인 가능

--- muldum/src/components/Modal/Login/login.tsx
+++ muldum/src/components/Modal/Login/login.tsx
@@ -9,26 +9,43 @@ export default function LoginModal({ onClose }: LoginModalProps) {
     const [email, setEmail] = useState('');
     const [pw, setPw] = useState('');
 
+    const handleLogin = () => {
+        if (email && pw) {
+            alert('환영합니다! 로그인에 성공했습니다.');
+            onClose();
+        } else {
+            alert('이메일과 비밀번호를 입력해주세요');
+        }
+    };
+
     return (
         <_.Overlay>
-        <_.Container>
-            <_.Title>로그인</_.Title>
+            <_.Container>
+                <_.Title>로그인</_.Title>
 
-            <_.InputWrap>
-            <img src={mail} alt="mail" />
-            <input type="email" placeholder="이메일을 입력하세요" value={email} onChange={(e) => setEmail(e.target.value)}
-            />
-            </_.InputWrap>
+                <_.InputWrap>
+                    <img src={mail} alt="mail" />
+                    <input
+                        type="email"
+                        placeholder="이메일을 입력하세요"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </_.InputWrap>
 
-            <_.InputWrap>
-            <img src={password} alt="password" />
-            <input type="password" placeholder="비밀번호" value={pw} onChange={(e) => setPw(e.target.value)}
-            />
-            </_.InputWrap>
+                <_.InputWrap>
+                    <img src={password} alt="password" />
+                    <input
+                        type="password"
+                        placeholder="비밀번호"
+                        value={pw}
+                        onChange={(e) => setPw(e.target.value)}
+                    />
+                </_.InputWrap>
 
-            <_.LoginButton>로그인</_.LoginButton>
-            <_.CloseButton onClick={onClose}>닫기</_.CloseButton>
-        </_.Container>
+                <_.LoginButton onClick={handleLogin}>로그인</_.LoginButton>
+                <_.CloseButton onClick={onClose}>닫기</_.CloseButton>
+            </_.Container>
         </_.Overlay>
     );
 }
