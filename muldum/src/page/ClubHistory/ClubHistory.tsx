ec7a1c0eb960c27548724f6da7c6bfb4d5615be4
dpqlsn <a01037111209@gmail.com>
2025-07-09 10:25:59 +0900

    feat (#2) :: 기수 버튼

--- muldum/src/page/ClubHistory/ClubHistory.tsx
+++ muldum/src/page/ClubHistory/ClubHistory.tsx
@@ -1,11 +1,19 @@
-import * as _ from './style'
-import NavBar from '@_components/NavBar/NavBar'
+import * as _ from "./style";
+import NavBar from "@_components/NavBar/NavBar";
+import arrow from '@_assets/icon/arrow.svg';
 
 export default function ClubHistory() {
     return (
         <_.Container>
-            <NavBar />
-            역대동아리
+        <NavBar />
+        <_.Wrapper>
+            <_.Title>역대 전공동아리</_.Title>
+            <_.Subtitle>이때까지 전공동아리 활동 전적을 확인해요</_.Subtitle>
+            <_.period>
+                <_.PeriodtText>3기</_.PeriodtText>
+                <_.arrow src={arrow} alt="arrow" />
+            </_.period>
+        </_.Wrapper>
         </_.Container>
-    )
+    );
 }
--- muldum/src/page/ClubHistory/style.ts
+++ muldum/src/page/ClubHistory/style.ts
@@ -3,3 +3,48 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
     width: 100%;
 `;
+
+export const Wrapper = styled.div`
+    width: 100%;
+    margin-left: 14.5%;
+`;
+
+export const Title = styled.div`
+    display: flex;
+    justify-content: left;
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: #1D1D1D;
+    margin-bottom: 1%;
+    margin-top: 2%;
+`;
+
+export const Subtitle = styled.div`
+    display: flex;
+    justify-content: left;
+    font-size: 1.2rem;
+    color: #707070;
+    margin-bottom: 3%;
+`;
+
+export const period = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: transparent;
+    border: 0.1rem solid #909090;
+    border-radius: 0.25rem;
+    padding: 0.4% 0.6%;
+    cursor: pointer;
+    font-family: 'Paperlogy';
+`;
+
+export const PeriodtText = styled.div`
+    font-size: 0.8rem;
+    font-weight: 500;
+    color: #909090;
+`;
+
+export const arrow = styled.img`
+    color: #909090;
+`;
