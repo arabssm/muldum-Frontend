212511cbbbe530e388f474d204724a822316012f
dpqlsn <a01037111209@gmail.com>
2025-07-09 20:36:40 +0900

    feat :: 모듈화

--- muldum/src/page/Item/Approval/LongShot.tsx
+++ muldum/src/page/Item/Approval/LongShot.tsx
@@ -1,13 +1,7 @@
 import * as _ from './style';
 import '@_styles';
 import { Props } from './type';
-
-const Reason = [
-    '구입처를 잘못 입력한 물품',
-    '구매 사유가 적절하지 않은 물품',
-    '가격이 과도하게 많이 나가는 물품',
-    '기타사유',
-];
+import { Reason } from './data';
 
 export default function RejectReasonSelector({ rejectReason, setRejectReason }: Props) {
     return (
