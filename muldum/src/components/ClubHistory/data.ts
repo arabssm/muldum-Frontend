5e743f5c44ad52fc2de99fe3a1a49430b70182a4
dpqlsn <a01037111209@gmail.com>
2025-07-09 16:12:08 +0900

    add :: 1~3기 데이터 추가

--- muldum/src/components/ClubHistory/data.ts
+++ muldum/src/components/ClubHistory/data.ts
@@ -1,4 +1,44 @@
 const topDataByPeriod = {
+    '1기': [
+        {
+            idx: 1,
+            level: "동아리",
+            name: "우최동",
+            explanation: "미니카 레이싱과 SW의 결합",
+        },
+        {
+            idx: 2,
+            level: "동아리",
+            name: "무지개떡",
+            explanation: "안면인식 키오스크",
+        },
+        {
+            idx: 3,
+            level: "동아리",
+            name: "낑기는 형제들",
+            explanation: "자율주행 배달",
+        },
+    ],
+    '2기': [
+        {
+            idx: 1,
+            level: "동아리",
+            name: "BELEU",
+            explanation: "택배 보관함 예약 서비스",
+        },
+        {
+            idx: 2,
+            level: "동아리",
+            name: "Blacklist",
+            explanation: "프로젝트 보안 컨설팅 및 취약점 진단 서비스",
+        },
+        {
+            idx: 3,
+            level: "동아리",
+            name: "Da'at",
+            explanation: "스팀펑크를 배경으로 한 2D픽셀 아트 게임",
+        },
+    ],
     '3기': [
         {
             idx: 1,
@@ -22,6 +62,130 @@ const topDataByPeriod = {
 };
 
 const bottomDataByPeriod = {
+    '1기': [
+        {
+            idx: 4,
+            level: "동아리",
+            name: "manenox",
+            explanation: "게임콘텐츠 -컨트롤러 결합",
+        },
+        {
+            idx: 5,
+            level: "동아리",
+            name: "야누스",
+            explanation: "학교 내 자동 수송 시스템",
+        },
+        {
+            idx: 6,
+            level: "동아리",
+            name: "쉬스테마",
+            explanation: "옷 대여 및 공유 웹앱 개발",
+        },
+        {
+            idx: 7,
+            level: "동아리",
+            name: "COIN",
+            explanation: "미니게임 모음 웹페이지",
+        },
+        {
+            idx: 8,
+            level: "동아리",
+            name: "T자형 인재",
+            explanation: "팀 협업 플랫폼",
+        },
+        {
+            idx: 9,
+            level: "동아리",
+            name: "오태식과 친구들",
+            explanation: "지문인식을 통한 학생 신원 감지와 편의시설 이용",
+        },
+        {
+            idx: 10,
+            level: "동아리",
+            name: "밤돌이로",
+            explanation: "공동체 내에서 특정한 일을 같이 할 사람을 구하는 웹",
+        },
+        {
+            idx: 11,
+            level: "동아리",
+            name: "JJOK",
+            explanation: "자격증에 관한 정보를 알려주는 웹앱",
+        },
+        {
+            idx: 12,
+            level: "동아리",
+            name: "김씨 소프트",
+            explanation: "멀티 어드벤쳐 PC 게임",
+        },
+        {
+            idx: 13,
+            level: "동아리",
+            name: "blessing software",
+            explanation: "내 아이디어를 실현시켜줄 사람을 매칭시켜주는 웹앱",
+        }
+    ],
+    '2기': [
+        {
+            idx: 4,
+            level: "동아리",
+            name: "DevCoop",
+            explanation: "매점 전자통장, 포털사이트 운영",
+        },
+        {
+            idx: 5,
+            level: "동아리",
+            name: "emergency",
+            explanation: "일상을 도와주는 웨어러블 ",
+        },
+        {
+            idx: 6,
+            level: "동아리",
+            name: "I.F",
+            explanation: "사용자 맞춤형 여행코스 추천앱",
+        },
+        {
+            idx: 7,
+            level: "동아리",
+            name: "INSERT",
+            explanation: "교내 플랫폼 서비스",
+        },
+        {
+            idx: 8,
+            level: "동아리",
+            name: "PaP",
+            explanation: "자동화 로봇",
+        },
+        {
+            idx: 9,
+            level: "동아리",
+            name: "Void Team(String game)",
+            explanation: "게임개발",
+        },
+        {
+            idx: 10,
+            level: "동아리",
+            name: "다:ON",
+            explanation: "10대 어휘력 향상 도우미 어플",
+        },
+        {
+            idx: 11,
+            level: "동아리",
+            name: "밤돌이로",
+            explanation: "입학 지원 플랫폼",
+        },
+        {
+            idx: 12,
+            level: "동아리",
+            name: "섹시한 자매들",
+            explanation: "아이디어 상품 판매를 위한 웹사이트",
+        },
+        {
+            idx: 13,
+            level: "동아리",
+            name: "웅이야",
+            explanation: "관심사 공유 및 친구 만들기(채팅 어플)",
+        },
+    ],
     '3기': [
         {
             idx: 4,
@@ -50,7 +214,7 @@ const bottomDataByPeriod = {
         {
             idx: 8,
             level: "동아리",
-            name: "Da’at",
+            name: "Da'at",
             explanation: "2D 게임 개발",
         },
         {
