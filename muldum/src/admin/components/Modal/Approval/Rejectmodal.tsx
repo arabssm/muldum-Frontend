1cd83cb5cb5c319ab2b23ee01f75e40be7a03f05
dpqlsn <a01037111209@gmail.com>
2025-07-16 15:44:01 +0900

    fix :: 모달 수정

--- muldum/src/admin/components/Modal/Approval/Rejectmodal.tsx
+++ muldum/src/admin/components/Modal/Approval/Rejectmodal.tsx
@@ -36,7 +36,7 @@ return (
     <_.Modal ref={modalRef}>
         <_.CheckIcon src={Reject} alt='CheckIcon' />
         <_.TextArea>
-            <_.Title>물품신청을 수락하였습니다</_.Title>
+            <_.Title>물품신청을 거절하였습니다</_.Title>
             <_.SubTitle>다른 곳을 누르거나 5초 동안 기다리면<br />메인화면으로 이동합니다</_.SubTitle>
             <_.counts>{counts}</_.counts>
         </_.TextArea>
